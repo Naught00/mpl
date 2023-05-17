bd20e1d502bf0b3bff16e54530437bb023734917
Mark Naughton <mark@marknaughton.com>
2023-05-17 12:55:17 +0100

    Basic lexing

--- mpl.c
+++ mpl.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
 
 enum Type {
 	OPERATOR,
 	INTEGER,
+	ASSIGNMENT,
+	IDENTIFIER,
 };
 
 struct Token {
 	enum Type type;
-	char x;
+	char *x;
 };
 
 typedef struct Token Token;
@@ -42,25 +45,68 @@ int main(int argc, char **argv) {
 
 	printf("%s\n", program);
 
-	int i, tokenc;
+	int i, tokenc, r;
 	char ch;
 	for (i = 0, tokenc = 0; i < program_length; i++) {
 		ch = program[i];
 
 		if (isdigit(ch)) {
-			Token token = {INTEGER, ch};
+			char *repr = malloc(10);
+			memset(repr, 1, 10);
+			r = 0;
+
+			while (isdigit(program[i])) {
+				repr[r] = program[i];
+				r++;
+				i++;
+			}
+			repr[r + 1] = '\0';
+
+			Token token = {INTEGER, repr};
 			tokens[tokenc] = token;
 			tokenc++;
-			printf("integer\n");
+
 		} else if (is_operator(ch)) {
-			Token token = {OPERATOR, ch};
+			char *repr = malloc(2);
+			repr[0] = ch;
+			repr[1] = '\0';
+
+			Token token = {OPERATOR, repr};
 			tokens[tokenc] = token;
 			tokenc++;
-			printf("operator\n");
+		} else if (ch == '=') {
+			char *repr = malloc(2);
+			repr[0] = ch;
+			repr[1] = '\0';
+
+			Token token = {ASSIGNMENT, repr};
+			tokens[tokenc] = token;
+			tokenc++;
+		} else if (isalpha(ch)) {
+			char *identifier = malloc(10);
+			memset(identifier, 1, 10);
+			r = 0;
+
+			while isalpha(program[i]) {
+				identifier[r] = program[i];
+				r++;
+				i++;
+			}
+			identifier[r + 1] = '\0';
+
+			Token token = {IDENTIFIER, identifier};
+			tokens[tokenc] = token;
+			tokenc++;
 		}
+
 	}
 	for (i = 0; i < tokenc; i++) {        
-		printf("%c\n", tokens[i].x);
+		switch (tokens[i].type) {
+		case INTEGER: printf("INTEGER: %s\n", tokens[i].x); break;
+		case ASSIGNMENT: printf("ASSIGNMENT: %s\n", tokens[i].x); break;
+		case OPERATOR: printf("OPERATOR: %s\n", tokens[i].x); break;
+		case IDENTIFIER: printf("IDENTIFIER: %s\n", tokens[i].x); break;
+		}
 	}
 	free(program);
 }
